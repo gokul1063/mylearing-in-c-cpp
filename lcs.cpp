8cab29afef7db5b554b9650f07869138954a241a
Gokul <gokulrajeshkumar1063@gmail.com>
2025-11-12 14:01:13 +0530

    update

--- lcs.cpp
+++ lcs.cpp
@@ -16,6 +16,13 @@ int lcs(string x, string y){
     }
   }
 
+  for (int i = 0 ; i < n ; i++){
+    for (int j = 0 ; j < m ;j++){
+      cout << dp[i][j] << " ";
+    }
+    cout << endl;
+  }
+
   return dp[n][m];
 }
 
